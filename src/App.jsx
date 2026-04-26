79deb488bb394e1ce3d0202cd2fb133dde530e3e
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-04-26 14:55:46 +0530

    updates-main&app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import User from './pages/User';
+import UserDetail from './pages/UserDetail';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<User />} />
-        <Route path="/users/:id" element={<UserDetail />} /> {/* Dynamic Route*/}
-        <Route path="*" element={<NotFound />} /> {/* 404 Page */}
-      </Routes>
+      <div className="page-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<User />} />
+          {/* Dynamic Route */}
+          <Route path="/users/:id" element={<UserDetail />} />
+          {/* 404 Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
 
-export default App
+export default App;
